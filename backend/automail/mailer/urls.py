35b514f229bc0c279bf8c742382ec128bd8ddb19
Siyad Savad <siyadsavad313@gmail.com>
2025-12-09 11:08:07 +0530

    urls added

---
+++ backend/automail/mailer/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import MailPlanList, MailPlanCreate, MailPlanDetail, MailPlanUpdate, RecipientList
+
+
+urlpatterns = [
+  path('plans/', MailPlanList.as_view(), name='mailplan-list'),
+  path('plans/create/', MailPlanCreate.as_view(), name='mailplan-create'),
+  path('plans/<int:pk>/', MailPlanDetail.as_view(), name='mailplan-detail'),
+  path('plans/<int:pk>/update/', MailPlanUpdate.as_view(), name='mailplan-update'),
+
+  path('recipients/', RecipientList.as_view(), name='recipient-list'),
+]
