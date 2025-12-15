c375268bfece951588417947cf161e1cc404a0d7
Siyad Savad <siyadsavad313@gmail.com>
2025-12-15 15:43:17 +0530

    finished

--- backend/automail/automail/settings.py
+++ backend/automail/automail/settings.py
@@ -140,13 +140,6 @@ SIMPLE_JWT = {
 }
 
 
-# CELERY_BEAT_SCHEDULE = {
-#     "run-mail-plans-every-minute": {
-#         "task": "mailer.tasks.check_and_run_plans",
-#         "schedule": 60.0,
-#     }
-# }
-
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
 
